f071d67d5dee97705b327a34e43cc112df139d25
Gamer025 <33846895+Gamer025@users.noreply.github.com>
2023-09-23 14:42:25 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# Text Randomizer
+![Rainworld Text Randomizer](thumbnail.png)  
+Rain World Text Randomizer
+
+Randomizes most text in the game with randomly choosen different text  
+**WARNING:** All UI elements will still serve their original purpose but their names / descriptions will be random!  
+Its recommened to backup your settings and/or use a different save slot in case you interact with the wrong elements potentially erasing your progress etc!  
+(or make sure you know what each button does from memory)
+
+### Help how do I disable this monstrosity
+A: Go the main menu. Press the 4th button from the top. Scroll through all the mods until you find this mods thumbnail. Disable that mod and restart the game.  
+B: Either unsubscribe from the mod on Steam or in case you manually installed it to remove the mod files in your mod folder  
---
+++ TextRandomizer.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using System.Diagnostics;
+using System;
+using System.Security.Permissions;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace TextRandomizer;
+
+[BepInPlugin(MOD_ID, "Text Randomizer", "1.0.0")]
+public class TextRandomizer : BaseUnityPlugin
+{
+    public const string MOD_ID = "Gamer025.TextRandomizer";
+   
+    public void OnEnable()
+    {
+        On.InGameTranslator.Translate += InGameTranslator_Translate;
+    }
+
+    private string InGameTranslator_Translate(On.InGameTranslator.orig_Translate orig, InGameTranslator self, string s)
+    {
+        orig(self, s);
+        return self.shortStrings.ElementAt(UnityEngine.Random.Range(0, self.shortStrings.Count - 1)).Value;
+    }
+}
